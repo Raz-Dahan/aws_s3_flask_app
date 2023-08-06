4cbca5a3d8ed9719591ed4fdfc5ce258e7af0a2c
Raz-Dahan <razdahan31@gmail.com>
2023-08-06 19:25:00 +0300

    Publish

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+from flask import Flask, render_template, request, send_from_directory
+from dotenv import load_dotenv
+import boto3
+
+load_dotenv()
+
+app = Flask(__name__)
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+REGION_NAME = 'eu-central-1'
+
+s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=REGION_NAME)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ""
+
+    if request.method == 'POST':
+        search_name = request.form['search_name']
+        file_key = f'{search_name}.jpg'
+
+        try:
+            tmp_file = tempfile.NamedTemporaryFile(delete=False)
+            s3_client.download_file(S3_BUCKET_NAME, file_key, tmp_file.name)
+            tmp_file.close()
+
+            return send_from_directory(tempfile.gettempdir(), os.path.basename(tmp_file.name), as_attachment=True)
+        except:
+            message = f'No luck finding {file_key}'
+
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
