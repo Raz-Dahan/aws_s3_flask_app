2e82890167740ad61730181eb56b08fb14565a47
Raz-Dahan <razdahan31@gmail.com>
2023-09-14 15:38:08 +0300

    Update dev environment

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 REGION="eu-central-1"
 INSTANCE_IP=$(aws ec2 describe-instances --region $REGION --filters Name=tag:tier,Values=app --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 RSA_Key="~/.ssh/raz-key.pem"
-ENV_Path="~/Documents/.env"
+ENV_Path=$(eval echo "~/Documents/.env")
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $RSA_Key $ENV_Path ubuntu@$INSTANCE_IP:/home/ubuntu
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $RSA_Key $ENV_Path ubuntu@$INSTANCE_IP:/home/ubuntu/
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $RSA_Key ubuntu@$INSTANCE_IP "
-sudo apt update -y
+sudo apt-get update -y
 if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Installing Git..."
+    echo 'Git is not installed. Installing Git...'
     sudo apt install git -y
 else
-    echo "Git is already installed."
+    echo 'Git is already installed.'
 fi
 if ! command -v docker &> /dev/null; then
     echo 'Docker is not installed. Installing Docker...'
-    sudo apt install docker.io -y
+    sudo apt-get install docker.io -y
     sudo systemctl start docker
     sudo systemctl enable docker
 else
@@ -24,27 +24,29 @@ else
 fi
 if ! command -v docker-compose &> /dev/null; then
     echo 'Docker Compose is not installed. Installing Docker Compose...'
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L 'https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     echo 'Docker Compose has been installed.'
 else
     echo 'Docker Compose is already installed.'
 fi
 if [ ! -d image_downloader ]; then
-    echo "Cloning repository..."
+    echo 'Cloning repository...'
     git clone https://github.com/Raz-Dahan/image_downloader.git
     cd image_downloader
+    git checkout dev  # Remove this line before merging to main
     cp /home/ubuntu/.env .env
     sudo docker-compose build
 else
-    echo "Repository directory already exists. Pulling latest changes..."
+    echo 'Repository directory already exists. Pulling latest changes...'
     cd image_downloader
+    git checkout dev  # Remove this line before merging to main
     if git pull --ff-only; then
-        echo "Changes pulled successfully."
+        echo 'Changes pulled successfully.'
         cp /home/ubuntu/.env .env
         sudo docker-compose build
     else
-        echo "No changes in the repository."
+        echo 'No changes in the repository.'
     fi
 fi
 if sudo docker ps | grep -q image_downloader_app; then
